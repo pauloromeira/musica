c5368b3a1deef583ba52d89cdd93086320efc1de
Paulo Romeira <paulo@pauloromeira.com>
2018-09-09 07:21:36 -0300

    Rename intervals to INTERVAL_NAMES and print intervals

--- intervals.py
+++ intervals.py
@@ -9,7 +9,7 @@ SCALES = {
     'chromatic': (1,) * 12,
 }
 
-INTERVALS = {
+INTERVAL_NAMES = {
     0: 'perfect unison',
     1: 'minor second',
     2: 'major second',
@@ -36,3 +36,6 @@ def semitones(notes, scale='major'):
         return _st(notes, scale)
     else:
         return [_st(note, scale) for note in notes]
+
+def interval_name(semitones):
+    return INTERVAL_NAMES.get(semitones, 'unknown')
--- synth.py
+++ synth.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import sounddevice as sd
 from utils import frequency, waveform, crossfade
-from intervals import semitones
+from intervals import semitones, interval_name
 
 
 rate = 44100
-duration = 1
+duration = .5
 
 sd.default.samplerate = rate
 
 for s in semitones(range(8)):
-    print(s)
+    print(s, interval_name(s))
     f = frequency(s)
     wave = waveform(f, rate, duration)
     crossfade(wave)
