7ac3321096978040f2076f5c8944d94d90866da6
Paulo Romeira <paulo@pauloromeira.com>
2018-09-10 00:02:55 -0300

    Simplify crossfade and remove linspace

--- utils.py
+++ utils.py
@@ -7,24 +7,20 @@ def frequency(steps, start=-9, base=440.0):
     return base * math.pow(2.0, (start + steps)/12.0)
 
 
-def linspace(start=0.0, stop=1.0, steps=50):
-    return (
-        start * (1-p) + stop * p for p in
-        (i/(steps-1) for i in range(steps))
-    )
+def crossfade(wave, percent=.2):
+    n = int(len(wave) * percent / 2)
+    n_sqrt = math.sqrt(n)
+    for i in range(n):
+        amp = math.sqrt(i) / n_sqrt
+        wave[i] *= amp
+        wave[-(i+1)] *= amp
 
 
-def crossfade(wave, percent=.01):
-    n = int(len(wave) * percent)
-    for i, j, p in zip(range(n), range(-1,-(n+1),-1), linspace(steps=n)):
-        wave[i] *= p
-        wave[j] *= p
-
-
-def waveform(frequency, rate, duration):
+def waveform(frequency, rate, duration=1, volume=1):
     count = math.ceil(rate * duration)
     return np.fromiter(
-        (math.sin(2.0 * math.pi * r * frequency / rate) for r in range(count)),
+        (math.sin(2.0 * math.pi * r * frequency / rate) * math.sqrt(volume)
+         for r in range(count)),
         dtype=np.float32,
         count=count
     )
